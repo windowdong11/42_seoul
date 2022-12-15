839b0339f18df31fa3f13089d66e018dbb6b648d
Donggyu Won <dowon@c2r5s1.42seoul.kr>
2022-12-15 19:34:08 +0900

    shit

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: dowon <dowon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 19:02:17 by dowon             #+#    #+#             */
-/*   Updated: 2022/12/09 23:11:22 by dowon            ###   ########.fr       */
+/*   Updated: 2022/12/15 19:27:05 by dowon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 int	ft_memcmp(const void *p1, const void *p2, size_t num)
 {
-	const unsigned char	*ptr1;
-	const unsigned char	*ptr2;
+	const char	*ptr1;
+	const char	*ptr2;
 
-	ptr1 = (unsigned char *)p1;
-	ptr2 = (unsigned char *)p2;
+	ptr1 = (char *)p1;
+	ptr2 = (char *)p2;
 	while (num--)
 	{
 		if (*ptr1 != *ptr2)
