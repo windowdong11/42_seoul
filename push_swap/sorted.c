2316cc99bd735c8f932f3feb1d2cf222855f0e33
WonDongGyu <windowdong11@naver.com>
2023-04-13 14:56:47 +0000

    WIP

--- push_swap/sorted.c
+++ push_swap/sorted.c
@@ -16,46 +16,56 @@ int	is_sorted(t_stack_ab *st, int (*cmp)(int, int), int count)
 	return (1);
 }
 
+static int	handle_sorted_b_bottom(t_stack_ab *st, const int total_size,
+		int (*cmp)(int, int), int (*rcmp)(int, int))
+{
+	int	idx;
+
+	if (is_sorted(st, cmp, total_size))
+	{
+		idx = 0;
+		while (idx++ < total_size)
+			st->pb(st, 1);
+		idx = 0;
+		while (idx++ < total_size)
+			st->rb(st, 1);
+		return (1);
+	}
+	else if (is_sorted(st, rcmp, total_size))
+	{
+		idx = 0;
+		while (idx++ < total_size)
+		{
+			st->pb(st, 1);
+			st->rb(st, 1);
+		}
+		return (1);
+	}
+	return (0);
+}
+
 int	handle_sorted(t_stack_ab *st, const int total_size, int (*cmp)(int, int),
 		int (*rcmp)(int, int), t_position dst)
 {
 	int	idx;
 
-	idx = 0;
 	if (dst == A_TOP && is_sorted(st, cmp, total_size))
 		return (1);
 	else if (dst == A_BOTTOM && is_sorted(st, rcmp, total_size))
 	{
+		idx = 0;
 		while (idx++ < total_size)
 			st->ra(st, 1);
 		return (1);
 	}
 	else if (dst == B_TOP && is_sorted(st, rcmp, total_size))
 	{
+		idx = 0;
 		while (idx++ < total_size)
 			st->pb(st, 1);
 		return (1);
 	}
 	else if (dst == B_BOTTOM)
-	{
-		if (is_sorted(st, cmp, total_size))
-		{
-			while (idx++ < total_size)
-				st->pb(st, 1);
-			idx = 0;
-			while (idx++ < total_size)
-				st->rb(st, 1);
-			return (1);
-		}
-		else if (is_sorted(st, rcmp, total_size))
-		{
-			while (idx++ < total_size)
-			{
-				st->pb(st, 1);
-				st->rb(st, 1);
-			}
-			return (1);
-		}
-	}
+		return (handle_sorted_b_bottom(st, total_size, cmp, rcmp));
 	return (0);
 }
