7cb58128fa0ca5b6b2dc28b91f2d795719821026
windowdong11 <windowdong11@naver.com>
2023-06-08 20:23:34 +0900

    src

--- fdf/src/utils_3d/utils_3d.h
+++ fdf/src/utils_3d/utils_3d.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vector_op.h                                        :+:      :+:    :+:   */
+/*   utils_3d.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dowon <dowon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 15:22:32 by dowon             #+#    #+#             */
-/*   Updated: 2023/06/06 18:38:30 by dowon            ###   ########.fr       */
+/*   Updated: 2023/06/07 17:09:04 by dowon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,48 @@
 # include "quaternion.h"
 
 /* matrix3 */
+t_matrix3		*m3_zero(t_matrix3 *out)
+{
+	int	idx;
+
+	idx = 0;
+	while (idx < 3)
+	{
+		out->m[idx][0] = 0;
+		out->m[idx][1] = 0;
+		out->m[idx][2] = 0;
+		idx++;
+	}
+	return (out);
+}
+t_matrix3		*m3_identity(t_matrix3 *out);
+t_matrix3		*m3_identity(t_matrix3 *out)
+{
+	out->_12 = 0;
+	out->_13 = 0;
+	out->_21 = 0;
+	out->_23 = 0;
+	out->_31 = 0;
+	out->_32 = 0;
+	return (out);
+}
 t_matrix3		*m3_multiply(t_matrix3 *out,
 					const t_matrix3 *m1, const t_matrix3 *m2);
 t_matrix3		*m3_translation(t_matrix3 *out, float x, float y, float z);
+t_matrix3		*m3_translation(t_matrix3 *out, float x, float y, float z)
+{
+    m3_identity(out);
+    out->_11 = x;
+    out->_21 = y;
+    out->_31 = z;
+    return (out);
+}
 t_matrix3		*m3_rotationX(t_matrix3 *out, float Angle);
 t_matrix3		*m3_rotationY(t_matrix3 *out, float Angle);
 t_matrix3		*m3_rotationZ(t_matrix3 *out, float Angle);
-t_matrix3		*m3_rotation_quaternion(t_matrix3 *out, const t_quaternion *pQ);
+/* t_matrix3		*m3_rotation_quaternion(t_matrix3 *out, const t_quaternion *pQ);
 t_matrix3		*m3_scaling(t_matrix3 *out, float sx, float sy, float sz);
-t_matrix3		*m3_affinetransform(t_matrix3 *out, float Scaling, const t_vector3 *pRotationCenter, const t_quaternion *pRotation, const t_vector3 *pTranslation);
+t_matrix3		*m3_affinetransform(t_matrix3 *out, float Scaling, const t_vector3 *pRotationCenter, const t_quaternion *pRotation, const t_vector3 *pTranslation); */
 /* vector3 */
 t_vector3		*v3_cross(t_vector3 *out, const t_vector3 *v1, const t_vector3 *v2);
 float			v3_dot(const t_vector3 *v1, const t_vector3 *v2);
