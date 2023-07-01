bbfe4ec95a60cba28a17d84fa45673da336c16e5
Donggyu Won <dowon@c2r5s1.42seoul.kr>
2023-07-01 23:16:22 +0900

    minishell

---
+++ minishell/src/builtins/echo.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dowon <dowon@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 22:55:04 by dowon             #+#    #+#             */
+/*   Updated: 2023/07/01 23:13:03 by dowon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtins.h"
+#include <stdio.h>
+#include <libft.h>
+
+t_exit_code	echo(int argc, char *argv[])
+{
+	size_t	idx;
+
+	if (argc == 1)
+	{
+		printf("\n");
+		return (0);
+	}
+	idx = 0;
+	if (ft_strncmp(argv[1], "-n"))
+		idx = 1;
+	while (idx + 1 < argc)
+		printf("%s ", argv[idx]);
+	printf("%s", argv[idx]);
+	if (!ft_strncmp(argv[1], "-n"))
+		printf("\n");
+	return (0);
+}
