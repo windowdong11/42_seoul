f2d0499d0019e39ea444f925a43cbec832f0bf93
Donggyu Won <dowon@c2r5s1.42seoul.kr>
2024-01-24 22:40:13 +0900

    irc

---
+++ irc/kqueue.cpp
@@ -0,0 +1,18 @@
+#include <sys/event.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <iostream>
+
+int main()
+{
+	// int fd = kqueue();
+	// if (fd == -1)
+	// {
+	// 	std::cout << "event queue creation failed" << std::endl;
+	// 	return 0;
+	// }
+	// struct kevent events[10];
+	// EV_SET(newEventFd, fd, EV, flags, fflags, data, udata);
+	// kevent(fd, )
+	return 0;
+}
