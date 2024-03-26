56aa6c0383440f1ce02141ea3f2b21a9043c4147
Donggyu Won <dowon@c2r5s1.42seoul.kr>
2024-03-26 14:26:48 +0900

    fix cpp06

--- cpp/cpp06/ex02/Base.cpp
+++ cpp/cpp06/ex02/Base.cpp
@@ -48,7 +48,7 @@ void identify(Base &p)
 {
 	try
 	{
-		dynamic_cast<A &>(p);
+		(void)dynamic_cast<A &>(p);
 		std::cout << "A" << std::endl;
 		return;
 	}
@@ -57,7 +57,7 @@ void identify(Base &p)
 	}
 	try
 	{
-		dynamic_cast<B &>(p);
+		(void)dynamic_cast<B &>(p);
 		std::cout << "B" << std::endl;
 		return;
 	}
@@ -66,7 +66,7 @@ void identify(Base &p)
 	}
 	try
 	{
-		dynamic_cast<C &>(p);
+		(void)dynamic_cast<C &>(p);
 		std::cout << "C" << std::endl;
 		return;
 	}
