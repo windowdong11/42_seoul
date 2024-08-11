7b58c6ad7993d6d7abfe4d5e89a377747f10c4f3
windowdong11 <windowdong11@naver.com>
2024-08-11 22:17:57 +0900

    inception add new user

--- inception/srcs/requirements/wordpress/install.sh
+++ inception/srcs/requirements/wordpress/install.sh
@@ -9,6 +9,7 @@ else
 	cd /var/www/html
 	wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --allow-root
 	wp core install --allow-root --url=${WP_DOMAIN} --title="inception-dowon" --admin_user=${WP_USER} --admin_password=${WP_PASSWORD} --admin_email=${WP_EMAIL}
+	wp user create ${USER_NAME} ${USER_EMAIL} --user_pass=${USER_PASSWORD}
 fi
 
 exec php-fpm7.4 -F
